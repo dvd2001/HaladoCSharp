031f3dd12219ec471530b900945dcb65a026ac20
dvd2001 <bedavid2001@gmail.com>
2026-03-24 18:53:32 +0100

    Detecting invalid board and win condition on selection instead of on game start

--- TikTakToe/TikTakToe/Menu.xaml.cs
+++ TikTakToe/TikTakToe/Menu.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace TikTakToe
 {
@@ -10,21 +12,19 @@ namespace TikTakToe
         private int size;
         private int win;
         private bool isMulti;
+        private bool isInit;
 
         public Menu()
         {
+            isInit = true;
             InitializeComponent();
+            isInit = false;
         }
 
         private void SinglePlayer_Click(object sender, RoutedEventArgs e)
         {
             size = int.Parse(cbSizeSelector.SelectionBoxItem.ToString().Split('x')[0]);
             win = int.Parse(cbWinSelector.SelectionBoxItem.ToString());
-            if (win>size)
-            {
-                MessageBox.Show("Invalid game parameters. The win condition cannot be greater than the size if the board.");
-                return;
-            }
             isMulti = false;
             Game game = new Game(size, win, isMulti);
             game.Show();
@@ -37,11 +37,6 @@ namespace TikTakToe
             
             size = int.Parse(cbSizeSelector.SelectionBoxItem.ToString().Split('x')[0]);
             win = int.Parse(cbWinSelector.SelectionBoxItem.ToString());
-            if(win>size)
-            {
-                MessageBox.Show("Invalid game parameters. The win condition cannot be greater than the size of the board.");
-                return;
-            }
             isMulti = true;
             Game game = new Game(size, win, isMulti);
             game.Show();
@@ -53,6 +48,29 @@ namespace TikTakToe
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void WinCondition_Changed(object sender, RoutedEventArgs e)
+        {
+            if (!isInit)
+            {
+                ComboBoxItem winSelector = cbWinSelector.SelectedValue as ComboBoxItem;
+                ComboBoxItem boardSelector = cbSizeSelector.SelectedValue as ComboBoxItem;
+                int boardSize = int.Parse(boardSelector.Content.ToString().Split('x')[0]);
+                int winBy = int.Parse(winSelector.Content.ToString());
+                Style style = new Style(typeof(ComboBox));
+                if (boardSize < winBy)
+                {
+                    MessageBox.Show("The win condition cannot be greater then the size of the board!");
+                    btnSinglePlayer.IsEnabled = false;
+                    btnMultiPlayer.IsEnabled = false;
+                }
+                else
+                {
+                    btnSinglePlayer.IsEnabled = true;
+                    btnMultiPlayer.IsEnabled = true;
+                }
+            }
         }
     }
 }
