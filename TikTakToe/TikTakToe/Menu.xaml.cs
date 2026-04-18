6c70ea650398f3e3ee844b8a8be5ddc95fceb603
dvd2001 <bedavid2001@gmail.com>
2026-04-18 16:07:38 +0200

    Refactoring the whole theme switch system

--- TikTakToe/TikTakToe/Menu.xaml.cs
+++ TikTakToe/TikTakToe/Menu.xaml.cs
@@ -16,16 +16,6 @@ namespace TikTakToe
         private bool isMulti;
         private bool isInit;
         private bool isDarkMode;
-        private Style cbDarkStyle;
-        private Style cbLightStyle;
-        private Style btnDarkStyle;
-        private Style btnLightStyle;
-        private Style txtbDarkStyle;
-        private Style txtbLightStyle;
-        private Style lblDarkStyle;
-        private Style lblLightStyle;
-        private Style cbxDarkStyle;
-        private Style cbxLightStyle;
 
         public Menu(string username, bool isDarkMode)
         {
@@ -34,10 +24,8 @@ namespace TikTakToe
             isInit = true;
             InitializeComponent();
             isInit = false;
-            DarkStyleSetUp();
-            LightStyleSetup();
-            SetupStyle();
             SetupSelections();
+            ((App)Application.Current).ThemeToggle(isDarkMode);
         }
         private void SetupSelections()
         {
@@ -47,136 +35,10 @@ namespace TikTakToe
                 cbSizeSelector.SelectedIndex = int.Parse(settings[0].Split('x')[0]) - 3;
                 cbWinSelector.SelectedIndex = int.Parse(settings[1]) - 3;
                 cbLineThickness.SelectedIndex = int.Parse(settings[2]) - 1;
-                cbxDarkMode.IsChecked = bool.Parse(settings[3]);
+                cbxDarkMode.IsChecked = isDarkMode = bool.Parse(settings[3]);
             }
         }
-        public void DarkStyleSetUp()
-        {
-            cbDarkStyle = new Style(typeof(ComboBox));
-            cbDarkStyle.Setters.Add(new Setter(ComboBox.BackgroundProperty, System.Windows.Media.Brushes.Black));
-            cbDarkStyle.Setters.Add(new Setter(ComboBox.ForegroundProperty, System.Windows.Media.Brushes.White));
-            btnDarkStyle = new Style(typeof(Button));
-            btnDarkStyle.Setters.Add(new Setter(Button.BackgroundProperty, System.Windows.Media.Brushes.DarkSlateGray));
-            btnDarkStyle.Setters.Add(new Setter(Button.ForegroundProperty, System.Windows.Media.Brushes.White));
-            btnDarkStyle.Resources.Add(typeof(Border), new Style(typeof(Border))
-            {
-                Setters =
-                {
-                    new Setter(Border.CornerRadiusProperty, new CornerRadius(10))
-                }
-            });
-            btnDarkStyle.Triggers.Add(new Trigger
-            {
-                Property = Button.IsEnabledProperty,
-                Value = false,
-                Setters =
-                {
-                    new Setter(Button.OpacityProperty, 0.5)
-                }
-            });
-            btnDarkStyle.Setters.Add(new Setter(Button.BorderThicknessProperty, new Thickness(0)));
-            btnDarkStyle.Setters.Add(new Setter(Button.PaddingProperty, new Thickness(10)));
-            btnDarkStyle.Setters.Add(new Setter(Button.MarginProperty, new Thickness(10)));
-            btnDarkStyle.Setters.Add(new Setter(Button.FontSizeProperty, 30.0));
-            txtbDarkStyle = new Style(typeof(TextBox));
-            txtbDarkStyle.Setters.Add(new Setter(TextBox.ForegroundProperty, System.Windows.Media.Brushes.White));
-            lblDarkStyle = new Style(typeof(Label));
-            lblDarkStyle.Setters.Add(new Setter(Label.ForegroundProperty, System.Windows.Media.Brushes.White));
-            cbxDarkStyle = new Style(typeof(CheckBox));
-            cbxDarkStyle.Setters.Add(new Setter(CheckBox.ForegroundProperty, System.Windows.Media.Brushes.White));
-        }
-
-        public void LightStyleSetup()
-        {
-            cbLightStyle = new Style(typeof(ComboBox));
-            cbLightStyle.Setters.Add(new Setter(ComboBox.BackgroundProperty, System.Windows.Media.Brushes.White));
-            cbLightStyle.Setters.Add(new Setter(ComboBox.ForegroundProperty, System.Windows.Media.Brushes.Black));
-            btnLightStyle = new Style(typeof(Button));
-            btnLightStyle.Setters.Add(new Setter(Button.BackgroundProperty, System.Windows.Media.Brushes.LightGray));
-            btnLightStyle.Setters.Add(new Setter(Button.ForegroundProperty, System.Windows.Media.Brushes.Black));
-            btnLightStyle.Resources.Add(typeof(Border), new Style(typeof(Border))
-            {
-                Setters =
-                {
-                    new Setter(Border.CornerRadiusProperty, new CornerRadius(10))
-                }
-            });
-            btnLightStyle.Setters.Add(new Setter(Button.BorderThicknessProperty, new Thickness(0)));
-            btnLightStyle.Setters.Add(new Setter(Button.PaddingProperty, new Thickness(10)));
-            btnLightStyle.Setters.Add(new Setter(Button.MarginProperty, new Thickness(10)));
-            btnLightStyle.Setters.Add(new Setter(Button.FontSizeProperty, 30.0));
-            txtbLightStyle = new Style(typeof(TextBox));
-            txtbLightStyle.Setters.Add(new Setter(TextBox.ForegroundProperty, System.Windows.Media.Brushes.Black));
-            lblLightStyle = new Style(typeof(Label));
-            lblLightStyle.Setters.Add(new Setter(Label.ForegroundProperty, System.Windows.Media.Brushes.Black));
-            cbxLightStyle = new Style(typeof(CheckBox));
-            cbxLightStyle.Setters.Add(new Setter(CheckBox.ForegroundProperty, System.Windows.Media.Brushes.Black));
-        }
-        private void SetupStyle()
-        {
-            if (isDarkMode)
-            {
-                cbxDarkMode.IsChecked = true;
-                this.Style = new Style(typeof(Window))
-                {
-                    Resources =
-                    {
-                        { typeof(ComboBox), cbDarkStyle },
-                        { typeof(Button), btnDarkStyle },
-                        { typeof(TextBox), txtbDarkStyle },
-                        { typeof(Label), lblDarkStyle },
-                        { typeof(CheckBox), cbxDarkStyle }
-                    },
-                    Setters =
-                    {
-                        new Setter(Window.BackgroundProperty, System.Windows.Media.Brushes.Black)
-                    }
-                };
-                cbSizeSelector.ItemContainerStyle = new Style(typeof(ComboBoxItem))
-                {
-                    Setters =
-                    {
-                        new Setter(ComboBoxItem.BackgroundProperty, System.Windows.Media.Brushes.DarkSlateGray),
-                        new Setter(ComboBoxItem.ForegroundProperty, System.Windows.Media.Brushes.White)
-                    }
-                };
-                cbWinSelector.ItemContainerStyle = new Style(typeof(ComboBoxItem))
-                {
-                    Setters =
-                    {
-                        new Setter(ComboBoxItem.BackgroundProperty, System.Windows.Media.Brushes.DarkSlateGray),
-                        new Setter(ComboBoxItem.ForegroundProperty, System.Windows.Media.Brushes.White)
-                    }
-                };
-                cbLineThickness.ItemContainerStyle = new Style(typeof(ComboBoxItem))
-                {
-                    Setters =
-                    {
-                        new Setter(ComboBoxItem.BackgroundProperty, System.Windows.Media.Brushes.DarkSlateGray),
-                        new Setter(ComboBoxItem.ForegroundProperty, System.Windows.Media.Brushes.White)
-                    }
-                };
-            }
-            else
-            {
-                this.Style = new Style(typeof(Window))
-                {
-                    Resources =
-                        {
-                            { typeof(ComboBox), cbLightStyle },
-                            { typeof(Button), btnLightStyle },
-                            { typeof(TextBox), txtbLightStyle },
-                            { typeof(Label), lblLightStyle },
-                            { typeof(CheckBox), cbxLightStyle }
-                        },
-                    Setters =
-                        {
-                            new Setter(Window.BackgroundProperty, System.Windows.Media.Brushes.White)
-                        }
-                };
-            }
-        }
-
+        
         private void SinglePlayer_Click(object sender, RoutedEventArgs e)
         {
             size = int.Parse(cbSizeSelector.SelectionBoxItem.ToString().Split('x')[0]);
@@ -241,74 +103,12 @@ namespace TikTakToe
         private void DarkMode_Checked(object sender, RoutedEventArgs e)
         {
             isDarkMode = true;
-            Style darkMode = new Style(typeof(Window));
-            darkMode.Resources.Add(typeof(ComboBox), cbDarkStyle);
-            darkMode.Resources.Add(typeof(Button), btnDarkStyle);
-            darkMode.Resources.Add(typeof(TextBox), txtbDarkStyle);
-            darkMode.Resources.Add(typeof(Label), lblDarkStyle);
-            darkMode.Resources.Add(typeof(CheckBox), cbxDarkStyle);
-            darkMode.Setters.Add(new Setter(Window.BackgroundProperty, System.Windows.Media.Brushes.Black));
-            this.Style = darkMode;
-            cbSizeSelector.ItemContainerStyle = new Style(typeof(ComboBoxItem))
-            {
-                Setters =
-                    {
-                        new Setter(ComboBoxItem.BackgroundProperty, System.Windows.Media.Brushes.DarkSlateGray),
-                        new Setter(ComboBoxItem.ForegroundProperty, System.Windows.Media.Brushes.White)
-                    }
-            };
-            cbWinSelector.ItemContainerStyle = new Style(typeof(ComboBoxItem))
-            {
-                Setters =
-                    {
-                        new Setter(ComboBoxItem.BackgroundProperty, System.Windows.Media.Brushes.DarkSlateGray),
-                        new Setter(ComboBoxItem.ForegroundProperty, System.Windows.Media.Brushes.White)
-                    }
-            };
-            cbLineThickness.ItemContainerStyle = new Style(typeof(ComboBoxItem))
-            {
-                Setters =
-                    {
-                        new Setter(ComboBoxItem.BackgroundProperty, System.Windows.Media.Brushes.DarkSlateGray),
-                        new Setter(ComboBoxItem.ForegroundProperty, System.Windows.Media.Brushes.White)
-                    }
-            };
+            ((App)Application.Current).ThemeToggle(isDarkMode);
         }
         private void DarkMode_Unchecked(object sender, RoutedEventArgs e)
         {
             isDarkMode = false;
-            Style lightMode = new Style(typeof(Window));
-            lightMode.Resources.Add(typeof(ComboBox), cbLightStyle);
-            lightMode.Resources.Add(typeof(Button), btnLightStyle);
-            lightMode.Resources.Add(typeof(TextBox), txtbLightStyle);
-            lightMode.Resources.Add(typeof(Label), lblLightStyle);
-            lightMode.Resources.Add(typeof(CheckBox), cbxLightStyle);
-            lightMode.Setters.Add(new Setter(Window.BackgroundProperty, System.Windows.Media.Brushes.White));
-            this.Style = lightMode;
-            cbSizeSelector.ItemContainerStyle = new Style(typeof(ComboBoxItem))
-            {
-                Setters =
-                    {
-                        new Setter(ComboBoxItem.BackgroundProperty, System.Windows.Media.Brushes.White),
-                        new Setter(ComboBoxItem.ForegroundProperty, System.Windows.Media.Brushes.Black)
-                    }
-            };
-            cbWinSelector.ItemContainerStyle = new Style(typeof(ComboBoxItem))
-            {
-                Setters =
-                    {
-                        new Setter(ComboBoxItem.BackgroundProperty, System.Windows.Media.Brushes.White),
-                        new Setter(ComboBoxItem.ForegroundProperty, System.Windows.Media.Brushes.Black)
-                    }
-            };
-            cbLineThickness.ItemContainerStyle = new Style(typeof(ComboBoxItem))
-            {
-                Setters =
-                    {
-                        new Setter(ComboBoxItem.BackgroundProperty, System.Windows.Media.Brushes.White),
-                        new Setter(ComboBoxItem.ForegroundProperty, System.Windows.Media.Brushes.Black)
-                    }
-            };
+            ((App)Application.Current).ThemeToggle(isDarkMode);
         }
     }
 }
